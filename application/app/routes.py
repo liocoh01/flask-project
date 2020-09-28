22136f6b1d4340b532ed85098b5be481b916a4ca
liorcohen <liocoh01@gmail.com>
2020-09-28 17:28:23 +0300

    Added get by last name

--- application/app/routes.py
+++ application/app/routes.py
@@ -1,21 +1,24 @@
 import connexion
 import pymysql.cursors
 from flask import jsonify
+import flask
 
 
-
-
-def get_people():
-    # Connect to the database
+def connect():
     connection = pymysql.connect(host=conf.config.get('DB_HOST'),
                                  user=conf.config.get('DB_USER'),
                                  password=conf.config.get('DB_PASS'),
                                  db=conf.config.get('DB'),
                                  charset='utf8mb4',
                                  cursorclass=pymysql.cursors.DictCursor)
+    return connection
 
+
+def get_people():
+    # Connect to the database
+    conn = connect()
     try:
-        with connection.cursor() as cursor:
+        with conn.cursor() as cursor:
             # Read a single record
             sql = "SELECT * FROM `Persons`"
             cursor.execute(sql)
@@ -23,10 +26,26 @@ def get_people():
             print(result)
             return jsonify(result)
     finally:
-        connection.close()
+        conn.close()
+
+
+def get_by_last_name(lastname):
+    # Connect to the database
+    conn = connect()
+    print(lastname)
+    try:
+        with conn.cursor() as cursor:
+            # Read a single record
+            sql = f"SELECT * FROM `Persons` WHERE  LastName = '{lastname}';"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            print(result)
+            return jsonify(result)
+    finally:
+        conn.close()
+
 
 app = connexion.FlaskApp(__name__, specification_dir='swagger/')
 app.add_api('swagger.yaml', arguments={'title': 'DB api'})
 conf = app.app
 conf.config.from_pyfile('app.conf')
-
